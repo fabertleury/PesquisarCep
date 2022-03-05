d05d36549b6b39bed6eb3af0af97922ac7828c94
fabertleury <fabert_@hotmail.com>
2022-03-05 10:32:46 -0300

    add global styles (styled components)

---
+++ src/styles/global.ts
@@ -0,0 +1,50 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: linear-gradient(252deg, rgba(153,41,234,1) 51%, rgba(88,8,251,1) 75%);
+    --backgroundalternative: linear-gradient(252deg, rgba(65,67,69,1) 51%, rgba(35,37,38,1) 75%);
+
+  }
+
+
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  html{
+    @media (max-width: 1080px) {
+      font-size: 93.75%; //15px
+    }
+
+    @media (max-width: 720px) {
+      font-size: 87.5%; //14px
+    }
+  }
+
+  body {
+    background: var(--backgroundalternative);
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: 'Roboto', sans-serif;
+    font-weight: 400;
+  }
+
+  h1, h2, h3, h4, h5, h6, strong {
+    font-weight: 600;
+  }
+
+  button{
+    cursor: pointer;
+  }
+
+  [disable] {
+    opacity: 0.6;
+    cursor: not-allowed;
+  }
+
+`
